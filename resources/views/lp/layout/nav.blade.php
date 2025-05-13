c8c0f3d1f5202b2727b24ee86a203aed7d3c3aca
Yuriowindiatmoko2401 <yuriowindiatmoko@gmail.com>
2025-05-13 12:57:04 +0700

    navbar akulturasi-6 using only md:text-white

--- resources/views/lp/layout/nav.blade.php
+++ resources/views/lp/layout/nav.blade.php
@@ -8,21 +8,21 @@
 
                 <!-- Desktop menu - centered -->
                 <ul class="hidden md:flex items-center justify-center space-x-8 mx-auto pl-0 mb-0 list-none">
-                    <li><a href="#admissions" class="text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">Admisi</a></li>
-                    <li><a href="#requirements" class="text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">Persyaratan</a></li>
-                    <li><a href="#process" class="text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">Proses</a></li>
-                    <li><a href="#tuition" class="text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">Biaya Kuliah</a></li>
-                    <li><a href="#faq" class="text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">FAQ</a></li>
+                    <li><a href="#admissions" class="md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">Admisi</a></li>
+                    <li><a href="#requirements" class="md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">Persyaratan</a></li>
+                    <li><a href="#process" class="md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">Proses</a></li>
+                    <li><a href="#tuition" class="md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">Biaya Kuliah</a></li>
+                    <li><a href="#faq" class="md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px] inline-block">FAQ</a></li>
                 </ul>
 
                 <!-- Login button -->
                 <div class="hidden md:block">
-                    <a href="#login" class="bg-transparent border-2 border-[#E2C665] text-white hover:bg-[#E2C665] hover:text-black text-[14px] transition py-1.5 px-4 rounded-full">Login</a>
+                    <a href="#login" class="bg-transparent border-2 border-[#E2C665] md:text-white hover:bg-[#E2C665] hover:text-black transition py-1.5 px-4 rounded-full">Login</a>
                 </div>
 
                 <!-- Mobile menu button -->
                 <button class="md:hidden focus:outline-none" id="menuBtn">
-                    <i data-feather="menu" class="text-white"></i>
+                    <i data-feather="menu" class="md:text-white"></i>
                 </button>
             </div>
 
@@ -30,14 +30,14 @@
             <div class="hidden md:hidden bg-gray-900" id="mobileMenu">
                 <div class="container mx-auto px-4 py-2">
                     <ul class="pl-0 mb-0 space-y-2 list-none">
-                        <li><a href="#admissions" class="block text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px]">Admisi</a></li>
-                        <li><a href="#requirements" class="block text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px]">Persyaratan</a></li>
-                        <li><a href="#process" class="block text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px]">Proses</a></li>
-                        <li><a href="#tuition" class="block text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px]">Biaya Kuliah</a></li>
-                        <li><a href="#faq" class="block text-white text-[14px] hover:text-[#E2C665] transition py-[10px] px-[10px]">FAQ</a></li>
+                        <li><a href="#admissions" class="block md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px]">Admisi</a></li>
+                        <li><a href="#requirements" class="block md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px]">Persyaratan</a></li>
+                        <li><a href="#process" class="block md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px]">Proses</a></li>
+                        <li><a href="#tuition" class="block md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px]">Biaya Kuliah</a></li>
+                        <li><a href="#faq" class="block md:text-white hover:text-[#E2C665] transition py-[10px] px-[10px]">FAQ</a></li>
                     </ul>
                     <div class="pt-2 mt-2 border-t border-gray-800">
-                        <a href="#login" class="block bg-transparent border-2 border-[#E2C665] text-[#E2C665] hover:bg-[#E2C665] hover:text-black text-[14px] transition py-1.5 px-4 my-2 rounded-full text-center">Login</a>
+                        <a href="#login" class="block bg-transparent border-2 border-[#E2C665] md:text-white hover:bg-[#E2C665] hover:text-black transition py-1.5 px-4 my-2 rounded-full text-center">Login</a>
                     </div>
                 </div>
             </div>
