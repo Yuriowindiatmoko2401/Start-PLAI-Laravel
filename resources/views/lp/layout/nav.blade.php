1a18fdfaeda1cb8c2fe53f9f71b7e08a67cf3fca
Yuriowindiatmoko2401 <yuriowindiatmoko@gmail.com>
2025-05-13 12:35:13 +0700

    navbar akulturasi-3 add login

--- resources/views/lp/layout/nav.blade.php
+++ resources/views/lp/layout/nav.blade.php
@@ -1,19 +1,23 @@
         <!-- Navigation -->
         <nav class="fixed w-full bg-black bg-opacity-90 border-b border-gray-800 z-50">
-        <div class="container px-4 md:px-0 flex flex-wrap items-center justify-between lg:flex-nowrap">
-            <div class="container mx-auto px-4 py-3 flex justify-between items-center">
+            <div class="container mx-auto px-4 py-3 flex items-center justify-between">
                 <!-- Image Logo -->
                 <a href="#" class="flex items-center">
-                    <img src="https://dev.plai.ac.id/assets/images/logo/PLAI-BMD%20-%20HORIZONTAL.png" alt="PLAI BMD Logo" class="h-8">
+                    <img src="https://dev.plai.ac.id/assets/images/logo/PLAI-BMD%20-%20HORIZONTAL.png" alt="PLAI BMD Logo" class="h-7">
                 </a>
 
-                <!-- Desktop menu -->
-                <div class="hidden md:flex space-x-8">
-                    <a href="#admissions" class="text-gray-300 hover:text-[#E2C665] transition">Admisi</a>
-                    <a href="#requirements" class="text-gray-300 hover:text-[#E2C665] transition">Persyaratan</a>
-                    <a href="#process" class="text-gray-300 hover:text-[#E2C665] transition">Proses</a>
-                    <a href="#tuition" class="text-gray-300 hover:text-[#E2C665] transition">Biaya Kuliah</a>
-                    <a href="#faq" class="text-gray-300 hover:text-[#E2C665] transition">FAQ</a>
+                <!-- Desktop menu - centered -->
+                <div class="hidden md:flex items-center justify-center space-x-8 mx-auto">
+                    <a href="#admissions" class="text-white text-sm hover:text-[#E2C665] transition">Admisi</a>
+                    <a href="#requirements" class="text-white text-sm hover:text-[#E2C665] transition">Persyaratan</a>
+                    <a href="#process" class="text-white text-sm hover:text-[#E2C665] transition">Proses</a>
+                    <a href="#tuition" class="text-white text-sm hover:text-[#E2C665] transition">Biaya Kuliah</a>
+                    <a href="#faq" class="text-white text-sm hover:text-[#E2C665] transition">FAQ</a>
+                </div>
+
+                <!-- Login button -->
+                <div class="hidden md:block">
+                    <a href="#login" class="bg-transparent border-2 border-[#E2C665] text-white hover:bg-[#E2C665] hover:text-black text-sm transition py-1.5 px-4 rounded-full">Login</a>
                 </div>
 
                 <!-- Mobile menu button -->
@@ -25,13 +29,15 @@
             <!-- Mobile menu -->
             <div class="hidden md:hidden bg-gray-900 border-b border-gray-800" id="mobileMenu">
                 <div class="container mx-auto px-4 py-2 space-y-3">
-                    <a href="#admissions" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Admisi</a>
-                    <a href="#requirements" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Persyaratan</a>
-                    <a href="#process" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Proses</a>
-                    <a href="#tuition" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Biaya Kuliah</a>
-                    <a href="#faq" class="block text-gray-300 hover:text-[#E2C665] transition py-2">FAQ</a>
+                    <a href="#admissions" class="block text-white text-sm hover:text-[#E2C665] transition py-2">Admisi</a>
+                    <a href="#requirements" class="block text-white text-sm hover:text-[#E2C665] transition py-2">Persyaratan</a>
+                    <a href="#process" class="block text-white text-sm hover:text-[#E2C665] transition py-2">Proses</a>
+                    <a href="#tuition" class="block text-white text-sm hover:text-[#E2C665] transition py-2">Biaya Kuliah</a>
+                    <a href="#faq" class="block text-white text-sm hover:text-[#E2C665] transition py-2">FAQ</a>
+                    <div class="pt-2 mt-2 border-t border-gray-800">
+                        <a href="#login" class="block bg-transparent border-2 border-[#E2C665] text-[#E2C665] hover:bg-[#E2C665] hover:text-black text-sm transition py-1.5 px-4 my-2 rounded-full text-center">Login</a>
+                    </div>
                 </div>
             </div>
-        </div>
         </nav>
         <!-- end of navigation -->
