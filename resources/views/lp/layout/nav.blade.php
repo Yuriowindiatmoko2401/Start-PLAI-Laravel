32c9c9ae04500e3319c9931e579e3a6f50427efc
Yuriowindiatmoko2401 <yuriowindiatmoko@gmail.com>
2025-05-13 12:25:33 +0700

    navbar akulturasi-2 edit biaya menu

--- resources/views/lp/layout/nav.blade.php
+++ resources/views/lp/layout/nav.blade.php
@@ -1,5 +1,6 @@
         <!-- Navigation -->
         <nav class="fixed w-full bg-black bg-opacity-90 border-b border-gray-800 z-50">
+        <div class="container px-4 md:px-0 flex flex-wrap items-center justify-between lg:flex-nowrap">
             <div class="container mx-auto px-4 py-3 flex justify-between items-center">
                 <!-- Image Logo -->
                 <a href="#" class="flex items-center">
@@ -11,7 +12,7 @@
                     <a href="#admissions" class="text-gray-300 hover:text-[#E2C665] transition">Admisi</a>
                     <a href="#requirements" class="text-gray-300 hover:text-[#E2C665] transition">Persyaratan</a>
                     <a href="#process" class="text-gray-300 hover:text-[#E2C665] transition">Proses</a>
-                    <a href="#scholarship" class="text-gray-300 hover:text-[#E2C665] transition">Beasiswa</a>
+                    <a href="#tuition" class="text-gray-300 hover:text-[#E2C665] transition">Biaya Kuliah</a>
                     <a href="#faq" class="text-gray-300 hover:text-[#E2C665] transition">FAQ</a>
                 </div>
 
@@ -27,9 +28,10 @@
                     <a href="#admissions" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Admisi</a>
                     <a href="#requirements" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Persyaratan</a>
                     <a href="#process" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Proses</a>
-                    <a href="#scholarship" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Beasiswa</a>
+                    <a href="#tuition" class="block text-gray-300 hover:text-[#E2C665] transition py-2">Biaya Kuliah</a>
                     <a href="#faq" class="block text-gray-300 hover:text-[#E2C665] transition py-2">FAQ</a>
                 </div>
             </div>
+        </div>
         </nav>
         <!-- end of navigation -->
